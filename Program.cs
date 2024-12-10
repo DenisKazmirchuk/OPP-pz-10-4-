9c0a32086bf543a37602d7d684e5b8a27122c887
DenisKazmirchuk <161360416+DenisKazmirchuk@users.noreply.github.com>
2024-12-10 20:37:28 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var domains = new List<string>
+        {
+            "google.com", "yahoo.com", "vk.com", "facebook.com",
+            "instagram.com", "twitter.com", "youtube.com",
+            "linkedin.com", "tiktok.com", "reddit.com"
+        };
+
+        var result = domains.FirstOrDefault(d => d == "vk.com");
+        Console.WriteLine(result != null ? $"Сайт знайдено: {result}" : "Сайт не знайдено");
+    }
+}
